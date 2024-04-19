cff0ab7c136c5c8aaeff9eaf49fa053e3612438b
Andrey Saderdinov <andreysaderdinov@gmail.com>
2024-04-19 18:18:11 +0300

    small update

--- add_to_bd.py
+++ add_to_bd.py
@@ -4,16 +4,29 @@ import sqlite3
 def add(name, surname):
     con = sqlite3.connect("bd.sqlite3")
     cur = con.cursor()
-    cur.execute(f"INSERT INTO users ({name}) VALUES ('{surname}')")
+    if name == "photo":
+        last = cur.execute(f"SELECT id, template_size FROM users").fetchall()[-1]
+        print(last[0], last[1])
+        if last[1] == "":
+            cur.execute(f"INSERT INTO users ({name}) VALUES ('{surname}')")
+        else:
+            cur.execute(f"UPDATE users set {name} = '{surname}' WHERE id = '{last[0]}'")
+    else:
+        last = cur.execute(f"SELECT id, photo FROM users").fetchall()[-1]
+        if last[1] == "":
+            cur.execute(f"INSERT INTO users ({name}) VALUES ('{surname}')")
+        else:
+            cur.execute(f"UPDATE users set {name} = '{surname}' WHERE id = '{last[0]}'")
     con.commit()
     cur.close()
     con.close()
 
 
-def take_values(id_user):
+def take_values():
     con = sqlite3.connect("bd.sqlite3")
     cur = con.cursor()
-    ans = cur.execute(f"SELECT photo, template_size FROM users WHERE id = '{id_user}'").fetchall()
+    last_id = cur.execute(f"SELECT id FROM users").fetchall()
+    ans = cur.execute(f"SELECT photo, template_size FROM users WHERE id = '{last_id[-1][0]}'").fetchall()
     return ans
 
 
@@ -23,3 +36,8 @@ def add_photo_to_bd(photo):
 
 def add_template(width, height):
     add("template_size", f"({width}, {height})")
+
+
+if __name__ == "__main__":
+    add("photo", "123")
+    add("template_size", "(10, 10)")
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from PyQt5 import QtWidgets
 from ui_form import Ui_MainWindow
 from add_to_bd import add_photo_to_bd, add_template, take_values
 from template import start
+from PyQt5.QtWidgets import QMessageBox
 
 
 class FlagMaker(QMainWindow):
@@ -12,19 +13,33 @@ class FlagMaker(QMainWindow):
         super().__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.ui.len_template_X.setMaxLength(4)
+        self.ui.len_template_Y.setMaxLength(4)
+        self.ui.len_template_X.setPlaceholderText("X: ")
+        self.ui.len_template_Y.setPlaceholderText("Y: ")
         self.ui.add_picture.clicked.connect(self.picture)
         self.ui.add_template.clicked.connect(self.resize_template)
         self.ui.start_programm.clicked.connect(self.start_programm)
+        self.widt = ""
+        self.heigh = ""
 
     def start_programm(self):
-        walues = take_values("14")[0]
-        picture = start(walues[0], walues[1])
-        self.ui.text_rating_3.setPixmap(QPixmap(picture))
+        if self.widt in ["", 0] and self.heigh in ["", 0]:
+            QMessageBox.about(self, "Ошибка", "Введите размеры листа!")
+        elif self.widt in ["", 0]:
+            QMessageBox.about(self, "Title", "Введите длину листа!")
+        elif self.heigh in ["", 0]:
+            QMessageBox.about(self, "Title", "Введите ширину листа!")
+        else:
+            walues = take_values()[0]
+            print(walues)
+            picture = start(walues[0], walues[1])
+            self.ui.text_rating_3.setPixmap(QPixmap(picture))
 
     def resize_template(self):
-        width = self.ui.len_template_X.text()
-        height = self.ui.len_template_Y.text()
-        add_template(width, height)
+        self.widt = self.ui.len_template_X.text()
+        self.heigh = self.ui.len_template_Y.text()
+        add_template(self.widt, self.heigh)
 
     def picture(self):
         self.add_picture = QtWidgets.QFileDialog.getOpenFileName(
